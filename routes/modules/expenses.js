8fe077b17a5c2f299fd6b98ffc80ea4d7cf1881e
deansyue <c1318131801@gmail.com>
2022-01-12 21:22:36 +0800

    modify: expenses router and edit & new page

--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -1,11 +1,19 @@
 //引入模組
 const express = require('express')
 
+const Category = require('../../models/Category')
+
 const router = express.Router()
 
 //新增費用支出，開啟表單頁面的路由
 router.get('/new', (req, res) => {
-  return res.render('new')
+  //查詢出全部的種類並在渲染new頁面時，動態產生類別下拉選項
+  Category.find({}, { name: 1 })
+    .lean()
+    .then((categories) => {
+      return res.render('new', { categories })
+    })
+
 })
 
 //新增費用支出的路由
@@ -15,7 +23,12 @@ router.post('/', (req, res) => {
 
 //修改特定費用支出，開啟修改頁面的路由
 router.get('/:id/edit', (req, res) => {
-  res.render('edit')
+  //查詢出全部的種類並在渲染edit頁面時，動態產生類別下拉選項
+  Category.find({}, { name: 1 })
+    .lean()
+    .then((categories) => {
+      return res.render('edit', { categories })
+    })
 })
 
 //修改特定費用支出的路由
